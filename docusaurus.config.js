c05a67c7a7fc4d3f36907877bc72b3fa08c737c7
Peter Schilling <peter@peterschilling.org>
2022-06-29 10:30:47 -0700

    Add basic docusaurus config for versions

---
+++ docusaurus.config.js
@@ -0,0 +1,11 @@
+// @ts-check
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Example",
+  url: "https://example.com",
+  baseUrl: "/",
+  presets: [["classic", {}]],
+};
+
+module.exports = config;
